451fa12160fd7df97327e52ff25d99187a642403
asmolen <artur.patryk.smolen@gmail.com>
2014-12-21 22:16:50 +0100

    Added getGraphFromFile() method

--- src/Graph.java
+++ src/Graph.java
@@ -1,22 +1,24 @@
 import java.util.List;
 import java.util.LinkedList;
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.AbstractMap;
 
 public class Graph {
 
-	private List<List<Integer>> adjList;
+	private List<List<Map.Entry<Integer, Integer>>> adjList;
 	private int _nodesNumber;
 	
 	
 	public Graph(int nodesNumber) {
 		_nodesNumber = nodesNumber;
-		adjList = new ArrayList<List<Integer>>(nodesNumber);
+		adjList = new ArrayList<List<Map.Entry<Integer, Integer>>>(nodesNumber);
 		
 		int nodeNr;
 		for (nodeNr = 1; nodeNr <= nodesNumber; nodeNr++)
 		{
-			List<Integer> list = new LinkedList<Integer>();
-			list.add(nodeNr);
+			List<Map.Entry<Integer, Integer>> list = new LinkedList<Map.Entry<Integer, Integer>>();			
+			list.add(new AbstractMap.SimpleEntry<>(nodeNr, 0));
 			adjList.add(list);
 		}
 	}
@@ -25,12 +27,12 @@ public class Graph {
 		return _nodesNumber;
 	}
 	
-	public void addEdge(Integer firstNode, Integer secondNode) {
-		adjList.get(firstNode - 1).add(secondNode);
-		adjList.get(secondNode - 1).add(firstNode);
+	public void addEdge(Integer firstNode, Integer secondNode, Integer weight) {
+		adjList.get(firstNode - 1).add(new AbstractMap.SimpleEntry<>(secondNode, weight));
+		adjList.get(secondNode - 1).add(new AbstractMap.SimpleEntry<>(firstNode, weight));
 	}
 	
-	public List<Integer> getNeightbours(Integer node) {
+	public List<Map.Entry<Integer, Integer>> getNeightbours(Integer node) {
 		return adjList.get(node - 1);
 	}
 }
--- src/GraphParser.java
+++ src/GraphParser.java
@@ -1,4 +1,7 @@
 import java.util.List;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class GraphParser {
 	
@@ -7,9 +10,66 @@ public class GraphParser {
 	}
 	
 	public Graph getGraphFromFile(String file) {
-		// TODO
+		final int COMMENT_CHAR = '#';
+		Scanner input;
+		String line = "";
+		int pos_comment;
+		Graph graph;
 		
-		return null;
+		try {
+			input = new Scanner(new File(file));
+		
+			// getting number of nodes
+			while (input.hasNextLine() && line.isEmpty())
+			{
+				line = input.nextLine();
+				pos_comment = line.indexOf(COMMENT_CHAR);
+				line = line.substring(0, pos_comment).trim();
+			}
+		
+			graph = new Graph(Integer.parseUnsignedInt(line));
+			
+			line = "";
+			// omitted list of terminals
+			while (input.hasNextLine() && line.isEmpty())
+			{
+				line = input.nextLine();
+				pos_comment = line.indexOf(COMMENT_CHAR);
+				line = line.substring(0, pos_comment).trim();
+			}
+		
+			// getting list of edges
+			while (input.hasNextLine())
+			{
+				line = "";
+				while (input.hasNextLine() && line.isEmpty())
+				{
+					line = input.nextLine();
+					pos_comment = line.indexOf(COMMENT_CHAR);
+					if (pos_comment != -1)
+					{
+						line = line.substring(0, pos_comment).trim();
+					}
+				}
+				
+				if (line.isEmpty())
+				{
+					break;
+				}
+			
+				String[] tab = line.split(" ");
+			
+				graph.addEdge(Integer.parseInt(tab[0]), Integer.parseInt(tab[1]), Integer.parseInt(tab[2]));
+			
+			}
+			input.close();
+		
+			return graph;
+		}
+		catch (FileNotFoundException e) {
+			e.getMessage();
+			return null;
+		}
 	}
 	
 	public void saveGraphToFile(Graph graph, String file) {
